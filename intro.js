59a3680463747dc36c52d33933103c6747cc53f0
bamtoll09 <ryan345@nate.com>
2024-09-23 20:35:28 +0900

    Added hover action and click event / Prevented drag and right click

--- intro.js
+++ intro.js
@@ -3,12 +3,19 @@ window.onload = () => {
 }
 
 var colours = ['red', 'blue', 'green', 'yellow', 'cyan', 'orange', 'aqua', 'gray', 'pink', 'purple', 'brown'];
-var divs = document.getElementsByTagName("div");
+
 
 function generateRandomColors() {
     var i;
-    for (i = 0; i < divs.length; i++) {
+    var tags = document.querySelectorAll("div, a");
+
+    for (i = 0; i < tags.length; i++) {
         var newColor = i % colours.length;
-        divs[i].style.backgroundColor = colours[newColor];
+
+        if (tags[i].tagName != "A")
+            tags[i].style.backgroundColor = colours[newColor];
+        tags[i].ondragstart = () => {return false;}
+        tags[i].ondrop = () => {return false;}
+        tags[i].addEventListener("contextmenu", (event) => {event.preventDefault();})
     }
 }
